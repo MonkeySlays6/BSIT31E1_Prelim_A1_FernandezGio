32458d78341b7c7b1b6c8bb45dea12e20c36b560
nigelchristian15 <nigelchristian76@gmail.com>
2026-06-13 09:35:02 +0800

    Prelim_A1_FernandezGio

---
+++ Prelim_A1_FernandezGio.cs
@@ -0,0 +1,25 @@
+﻿// FizzBuzz - Starter
+// TODO: Write a program that prints numbers 1 to 100.
+// For multiples of 3, print "Fizz" instead of the number.
+// For multiples of 5, print "Buzz".
+// For multiples of both 3 and 5, print "FizzBuzz".
+
+for (int i = 1; i <= 100; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine("FizzBuzz");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine("Fizz");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine("Buzz");
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
+}
